8b87c1851b7ac99593e76fcba8b40a558ed4f7a0
Spagliardin <spagliardi.n@gmail.com>
2021-02-01 13:27:01 -0300

    adding objets js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let invoiceValue = parseFloat(prompt('Ingrese el valor de la factura'));
+let invoiceValue = parseFloat(prompt('Ingrese el valor de la factura en divisa'));
 let quotationDay = 91.5;
 
 
@@ -7,3 +7,28 @@ function divide (value, quotation) {
 }
 
 alert(divide(invoiceValue, quotationDay));
+
+// objeto fecha
+
+let day = parseInt(prompt('Ingrese dia'));
+let month = parseInt(prompt('Ingrese mes'));
+let year = parseInt(prompt('ingrese Año'));
+
+function Date(day, month, year){
+    this.day = day
+    this.month = month
+    this.year = year
+}
+
+let invoiceDate = new Date(day, month, year)
+console.log(invoiceDate)
+
+// objeto Moneda 
+
+function Coin(exchangeRate, wholesaler){
+    this.exchangeRate = exchangeRate
+    this.wholesaler = wholesaler
+}
+
+let typeOfCoin = new Coin('Dolar', 'Mayorista');
+
