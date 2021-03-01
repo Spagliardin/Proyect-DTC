6c20df0222cdaea69c49d3a8e910c131261066aa
Spagliardin <spagliardi.n@gmail.com>
2021-02-28 23:06:51 -0300

    add jQuery in proyect

--- js/functions.js
+++ js/functions.js
@@ -2,63 +2,63 @@ function setValueCoin(){
     // Me traigo el valor del tipo de cambio
     let valueCoin = getValueCoin()
     // Tomo el elemento tipo de cambio de factura y le agrego el atributo valor del objeto
-   document.getElementById('tc-Invoice').setAttribute('value', valueCoin)
+    $('#tc-Invoice').val(valueCoin)
 }
 
 function getValueCoin(){
     // Uso de objeto para darle valor literal al tipo de cambio
     let valueCoin = new TypeCoin(90, 100, 1220)
     // Me traigo el texto del select
-    let typeOfCoin = document.getElementById('selectCoin');
-    typeOfCoin = typeOfCoin.options[typeOfCoin.selectedIndex].text;
+    
+    let typeOfCoin = $('#selectCoin option:selected').val().toUpperCase()
+
     // Valído el tipo de string que es el Select
-        if (typeOfCoin == 'Dolar'){
+        if (typeOfCoin == 'DOLAR'){
             typeOfCoin = valueCoin.dolar
         }
-        else if(typeOfCoin == 'Euro'){
+        else if(typeOfCoin == 'EURO'){
             typeOfCoin = valueCoin.euro
         }
-        else if(typeOfCoin == 'Real'){
+        else if(typeOfCoin == 'REAL'){
             typeOfCoin = valueCoin.real
         }
     return typeOfCoin
 }
 
+
 function setValueInvoiceInPesos (){
-    let valueInvoice = document.getElementById('value-Invoice');
-    let userValueInvoice = valueInvoice.value;
+
+    let userValueInvoice =  $('#value-Invoice').val();
 
     let valueCoin = getValueCoin();
 
-    let resultInvoicedInPesos = getResultInvoicedInPesos(valueCoin, userValueInvoice)
+    let resultInvoicedInPesos = getInvoicedResultInPesos(valueCoin, userValueInvoice)
 
-    document.getElementById('invoice-Pesos').setAttribute('value', resultInvoicedInPesos)
+    $('#invoice-Pesos').val(resultInvoicedInPesos)
 }
 
-function getResultInvoicedInPesos(valueInvoice, valueCoin){
+function getInvoicedResultInPesos(valueInvoice, valueCoin){
     return valueInvoice * valueCoin
 }
 
 function setAmountPay(){
-    let valueCoinPay = document.getElementById('value-Coin-Pay').value
-    let valueInvoice = document.getElementById('value-Invoice');
-    let userValueInvoice = valueInvoice.value;
+    let valueCoinPay = $('#value-Coin-Pay').val()
+    let userValueInvoice = $('#value-Invoice').val();
+    
    
-    let valueInvoicePay = getResultInvoicedInPesos(userValueInvoice, valueCoinPay)
+    let valueInvoicePay = getInvoicedResultInPesos(userValueInvoice, valueCoinPay)
 
-    document.getElementById('value-Pay').setAttribute('value', valueInvoicePay)
+    $('#value-Pay').val(valueInvoicePay)
 }
 
 function ndGenerate (){
-    let valuePayInvoice = document.getElementById('value-Pay').value;
-    let amountPaid = document.getElementById('amount-Paid')
-    let userAmountPaid = amountPaid.value;
+    let valuePayInvoice = $('#value-Pay').val();
+    let userAmountPaid = $('#amount-Paid').val()
+    
 
     let resultNd = deduct(valuePayInvoice, userAmountPaid)
 
-    let spancontent = document.createTextNode(resultNd)
-
-    let textND = document.getElementById('ND').appendChild(spancontent)
+    let textND = $('#ND').text(resultNd)
 }
 
 function deduct (valuePayInvoice, userAmountPaid){
--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
 
 // Tomar el valor de select y agregar el resultado (mediante evento) en el input tipo de cambio
-let typeCoin = document.getElementById('selectCoin').addEventListener('change', setValueCoin)
+$('#selectCoin').change(setValueCoin)
 
 // Tomar el valor de la factura y añadir evento de calculo para valor factura en pesos
 
-let valueInvoice = document.getElementById('value-Invoice').addEventListener('change', setValueInvoiceInPesos)
+$('#value-Invoice').change(setValueInvoiceInPesos)
 
 // Por el momento TC dia Abonado toma el valor por defecto del htm (150)
 
 // Tomar el valor de la factura y Obtener el monto correspondiente a abonar
 
-let amountPay = document.getElementById('value-Invoice').addEventListener('change', setAmountPay)
-
+$('#value-Invoice').change(setAmountPay)
 // Toma el valor de factura en pesos y el valor correspondiente, restalos y crea una nota de debito
 
-let valuePayInvoice = document.getElementById('amount-Paid').addEventListener('change', ndGenerate)
+$('#amount-Paid').change(ndGenerate)
 
